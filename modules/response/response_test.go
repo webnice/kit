0323594a13e0648c5fe8e036c6e7a7ab027be727
Alex Geer <monoflash@gmail.com>
2018-05-04 01:36:09 +0300

    Moves from web

---
+++ modules/response/response_test.go
@@ -0,0 +1,37 @@
+package response
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestNormalizeArrayIfNeeded(t *testing.T) {
+	var nilArr []int
+	var a, b, c = int(1), int(2), int(3)
+	cases := []struct {
+		a interface{}
+		b string
+	}{
+		{[]int{1, 2}, `[1,2]`},
+		{[]int{}, `[]`},
+		{nilArr, `[]`},
+		{nil, `null`},
+		{"hello", `"hello"`},
+		{map[string]interface{}{"hello": "hi!"}, `{"hello":"hi!"}`},
+		{map[string][]string{"hello": {"world", "!"}}, `{"hello":["world","!"]}`},
+		{map[string][]string{"hello": {}}, `{"hello":[]}`},
+		{&map[string][]string{"hello": {}}, `{"hello":[]}`},
+		{[]*int{&a, &b, &c}, `[1,2,3]`},
+		{&[]*int{&a, &b, &c}, `[1,2,3]`},
+		{[]*int{}, `[]`},
+		{&nilArr, `[]`},
+	}
+
+	for _, oneCase := range cases {
+		nVal := normalizeArrayIfNeeded(oneCase.a)
+		res, _ := json.Marshal(nVal)
+		if string(res) != oneCase.b {
+			t.Errorf("a: %#v; expected: %#v; got: %#v", oneCase.a, oneCase.b, string(res))
+		}
+	}
+}
---
+++ modules/verify/register.go
@@ -0,0 +1,26 @@
+package verify
+
+//import "gopkg.in/webnice/debug.v1"
+//import "gopkg.in/webnice/log.v2"
+import "gopkg.in/webnice/web.v1/context"
+import ()
+
+func init() {
+	// Регистрация внешней функции проверки данных
+	context.New(nil).
+		RegisterVerifyPlugin(new(webContextVerify))
+}
+
+type webContextVerify struct{}
+
+// Verify Implementation of context.VerifyPlugin
+func (wcv *webContextVerify) Verify(data interface{}) (rsp []byte, err error) {
+	var vfi Interface
+
+	if vfi, err = Verify(data); err != nil {
+		rsp = vfi.Message(err.Error()).Json()
+		return
+	}
+
+	return
+}
