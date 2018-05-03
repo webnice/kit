70b875dcd6fda344e39054e9564ada7b97fb186c
Alex Geer <monoflash@gmail.com>
2018-05-04 02:21:09 +0300

    Updated verify interface

--- modules/verify/register.go
+++ modules/verify/register.go
@@ -7,7 +7,7 @@ import ()
 
 func init() {
 	// Регистрация внешней функции проверки данных
-	context.New().RegisterVerifyPlugin(new(webContextVerify))
+	context.RegisterGlobalVerifyPlugin(new(webContextVerify))
 }
 
 type webContextVerify struct{}
@@ -15,11 +15,20 @@ type webContextVerify struct{}
 // Verify Implementation of context.VerifyPlugin
 func (wcv *webContextVerify) Verify(data interface{}) (rsp []byte, err error) {
 	var vfi Interface
-
 	if vfi, err = Verify(data); err != nil {
 		rsp = vfi.Message(err.Error()).Json()
 		return
 	}
+	return
+}
 
+// Verify Implementation of context.VerifyPlugin
+func (wcv *webContextVerify) Error400(err error) (rsp []byte) {
+	const defaultError = `Unknown error`
+	if err != nil {
+		rsp = E4xx().Code(-1).Message(err.Error()).Json()
+	} else {
+		rsp = E4xx().Code(-1).Message(defaultError).Json()
+	}
 	return
 }
